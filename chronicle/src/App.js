169d838b6aad9db5f056db0e28898a02d1531d90
bhavya2prakash <prakash.b@northeastern.edu>
2022-12-03 17:37:08 -0500

    added home page

--- chronicle/src/App.js
+++ chronicle/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Home from './Pages/Home';
+import Home from './pages/Home';
 
 
 function App() {
