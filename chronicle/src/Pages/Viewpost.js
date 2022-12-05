31d5105b90daebdfa074cb9a3055ed65f1159280
DeepShah1108 <shah.deepaj@northeastern.edu>
2022-12-05 17:04:41 -0500

    Star Rating and Review

--- chronicle/src/Pages/Viewpost.js
+++ chronicle/src/Pages/Viewpost.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/Viewpost.css'
+import { Rating, Button, TextField} from '@mui/material'
 import {Container,Row , Col} from 'react-bootstrap';
 function Viewpost() {
-    
+  const [ value, setValue ] = useState("5")
   return (
  <Container>
    
@@ -22,15 +23,46 @@ function Viewpost() {
         <p className="card-text"> Address:</p>
         <p className="card-text"> Description:</p>
         <p className="card-text"> Category:</p>
-        <p className="card-text"> Total Ratings:
-  
-      
+        <p className="card-text"> Total Ratings
         </p>
         <p className="card-text"><small className="text-muted">Date of created</small></p>
       </div>
     </div>
   </div>
 </div>
+
+<div className='row'>
+  <div className="col-md-4">
+        <div className="card-body">
+          <h6 className="card-title">Leave a Rating</h6>
+        </div>
+        <Rating sx={{mt: 2}}
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue )  => {
+            setValue(newValue);
+          }}
+        />
+  </div>
+  <div className="col-md-8">
+        <div className="card-body">
+          <h6 className="card-title">Leave a Review</h6>
+        </div>
+        <TextField sx={{mt: 2, width: "50%"}}
+          hiddenLabel
+          id="filled-hidden-label-normal"
+          placeholder='Review... '
+          size='small'
+        />
+        <Button sx={{ml: 3, mt: 2, backgroundColor: "#DC143C"}}
+        variant="contained">Submit</Button>
+  </div> 
+</div>
+
+
+
+
+
 
 </Container>
 
