9d562a23897cd97f5e80bff0e67c531ff979852c
DeepShah1108 <shah.deepaj@northeastern.edu>
2022-12-05 21:25:08 -0500

    Navbar

--- chronicle/src/Pages/loginPage/Form.js
+++ chronicle/src/Pages/loginPage/Form.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import userapi from "../../apiservice/userapi";
 
 import {
@@ -15,8 +15,18 @@ import {
 import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 import Dropzone from "react-dropzone";
 
+import { UserContext } from "../../App";
+import { Navigate } from "react-router-dom";
+
+
+
 
 const Form = () => {
+
+  
+
+  const { state, dispatch } = useContext(UserContext)
+
   const [pageType, setPageType] = useState("login");
   const [value, setValue] = useState("")
 
@@ -39,6 +49,10 @@ const Form = () => {
       alert(response.data.user);
       console.log(response.data.user);
       sessionStorage.setItem('user',response.data.user);
+      dispatch({type: "USER", payload: true})
+      return(
+        <Navigate to='/dashboard' />
+      )
     } catch (error) {
      alert("Unauthorized"); 
      //handle unauthorize scenerio
